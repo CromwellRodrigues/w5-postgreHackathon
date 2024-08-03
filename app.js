3fcd236717813279d7726d74ad44f70ff0ca9bd8
Crom <daffodil.luck9@gmail.com>
2024-08-03 23:42:47 +0100

    starting files

--- app.js
+++ app.js
@@ -36,105 +36,26 @@ app.use(helmet()); // use helmet to secure application
 // Endpoint to retrieve all artists
 app.get("/artist/", async function (req, res) {
 
-    try {
-
-      const result = await getArtists() // Invoke function to get all artists
-      res.status(200).json({ success: true, payload: result }) // Send a response with the artists if successful
-    
-    } catch(error) {
-
-      res.status(500).json({ success: false, payload: error}) // Handle any errors, giving back the error message
-    
-    }
 });
 
 // Endpoint to retrieve an artist by id
 app.get("/artist/:id", async function (req, res) {
 
-  try {
-
-    const result = await getArtistById(req.params.id) // Invoke function to get specific artist- ID will come from the url so we need to access the params object from the req
-    
-    if (!result) {
-      res.status(404).json({ success: false, payload: "No artist with that id was found"}) // If result is empty- then the user gave an invalid id
-      return // return is needed to break out of the request listener
-    }
-
-    res.status(200).json({ success: true, payload: result }) // Send a response with the artist if successful
-
-  } catch(error) {
-
-    res.status(500).json({ success: false, payload: error}) // Handle any errors, giving back the error message
-
-  }
 });
 
 // Endpoint to create a new artist
 app.post("/artist/", async function (req, res) {
 
-  try {
-
-    const result = await createArtist(req.body) // Invoke function to get specific artist- artist will come from the body so we need to access the body object from the req
-    
-    if (!req.body.name) {
-      res.status(404).json({ success: false, payload: "Please provide a name for the updated artist"}) // If result is empty- then the user did not give a name in the request
-      return // return is needed to break out of the request listener
-    }
-    
-    res.status(200).json({ success: true, payload: result }) // Send a response with the artist if successful
-
-  } catch(error) {
-
-    res.status(500).json({ success: false, payload: error}) // Handle any errors, giving back the error message
-
-  }
 });
 
 // Endpoint to update a specific artist by id
 app.patch("/artist/:id", async function (req, res) {
 
-  try {
-
-    const result = await updateArtistById(req.params.id, req.body) // Invoke function to update artist- artist will come from the body so we need to access the body object from the req, id will come from the url so we need to access the params object from the req
-    
-    if (!req.body.name) {
-      res.status(404).json({ success: false, payload: "Please provide a name for the updated artist"}) // If result is empty- then the user did not give a name in the request
-      return // return is needed to break out of the request listener
-    }
-
-    if (!result) {
-      res.status(404).json({ success: false, payload: "No artist with that id was found"}) // If result is empty- then the user gave an invalid id
-      return // return is needed to break out of the request listener
-    }
-
-    res.status(200).json({ success: true, payload: result }) // Send a response with the artist if successful
-
-  } catch(error) {
-
-    res.status(500).json({ success: false, payload: error}) // Handle any errors, giving back the error message
-
-  }
 });
 
 // Endpoint to delete a specific artist by id
 app.delete("/artist/:id", async function (req, res) {
 
-  try {
-
-    const result = await deleteArtistById(req.params.id) // Invoke function to delete artist- ID will come from the url so we need to access the params object from the req
-    
-    if (!result) {
-      res.status(404).json({ success: false, payload: "No artist with that id was found"}) // If result is empty- then the user gave an invalid id
-      return // return is needed to break out of the request listener
-    }
-    
-    res.status(200).json({ success: true, payload: result }) // Send a response with the artist if successful
-
-  } catch(error) {
-
-    res.status(500).json({ success: false, payload: error}) // Handle any errors, giving back the error message
-
-  }
 });
 
 
@@ -143,62 +64,17 @@ app.delete("/artist/:id", async function (req, res) {
 // Endpoint to retrieve all albums
 app.get("/album/", async function (req, res) {
 
-  try {
-    const result = await getAlbums(); // to get all albums from albums.js
-
-    res.status(200).json({ success: true, payload: result });
-  }  // sends back a response with all the albums
-
-  catch (error) {
-
-    
-    res.status(500).json ({success : false, payload: error}); // handle errors and give back the error message.
-  }
 });
   
 // Endpoint to retrieve a album  by id
 app.get("/album/:id", async function (req, res) {
-    
-  try {
-    const result = await getAlbumById(req.params.id);
-    // invoke function to get specific album from the url so that we can access the particular album.
-
-    if (!result) {
-      res.status(404).json({ success: false, payload: "No album with that id was found" });
-
-      return;
-      
-    }
-
-    res.status(200).json({ success: true, payload: result }); // send back a response with the particular artist details
-
-
-  }  catch (error) {
-
-    res.status(500).json({ success: false, payload: error }) 
-    } //handles any errors giving back the error in the message.
-
+  
 });
   
 // Endpoint to create a new album
 app.post("/album/", async function (req, res) {
 
-  try {
 
-const result = await createAlbum(req.body)//invoke a specific album - this album will come from the body within the post req
-
-if (!req.body.title, !req.body.published_date, !req.body.artist_id) {
-  res.status(404).json({ success: false, payload: "Please provide all information for the new ALbum"})// if the result is empty then the user didn't give a valid req for the album
-  return // return is needed to break out of the event listener.
-}
-
-res.status(200).json({sucecess: true, payload: result}) //send a resposne with the new album if successful
-
-  } catch(error) {
-
-res.status(500).json({success: false, payload: error}) // handle any errors - give back an error message if so. 
-
-  }
 });
 
 // Endpoint to update a specific album by id
--- artists.js
+++ artists.js
@@ -4,93 +4,27 @@ import { pool } from "./db/index.js";
 
 export async function getArtists() {
   // Query the database and return all artists
-
-  const queryString = 'SELECT * FROM artists' // Create a variable that will hold the SQL query
-
-  try {
-
-    const result = await pool.query(queryString) // Use pool.query to send the query to the DB
-    return result.rows // Return the rows from the result
-
-  } catch (error) {
-
-    throw new Error("There was an issue retrieving results: " + error) // Handle any error if the request doesn't go through
-  
-  }
+ 
 }
 
 export async function getArtistById(id) {
   // Query the database and return the artist with a matching id or null
 
-  const queryString = 'SELECT * FROM artists WHERE id = $1' // Create a variable that will hold the SQL query, the id will come from the url and is handed into the function when called
-
-  try {
-
-    const result = await pool.query(queryString, [id]) // Use pool.query to send the query to the DB, ID is taken from the function argument
-    return result.rows[0] // Return the row from the result
-
-  } catch (error) {
-
-    throw new Error("There was an issue retrieving results: " + error) // Handle any error if the request doesn't go through
-  
-  }
 }
 
 export async function createArtist(artist) {
   // Query the database to create an artist and return the newly created artist
 
-  const queryString = 'INSERT INTO artists (name) VALUES ($1) RETURNING *' // Create a variable that will hold the SQL query, the name will come from the body and is handed into the function when called. RETURNING * is required for the query to return a result
-
-  try {
-
-    if (!artist.name) {
-      return // This will stop the function from executing if the user did not provide a name
-    }
-
-    const result = await pool.query(queryString, [artist.name]) // Use pool.query to send the query to the DB, name is taken from artist in the function argument
-    return result.rows[0] // Return the row from the result
-
-  } catch (error) {
-
-    throw new Error("There was an issue creating artist: " + error) // Handle any error if the request doesn't go through
-  
-  }
+ 
 }
 
 export async function updateArtistById(id, artist) {
   // Query the database to update the resource and return the newly updated artist or null
 
-  const queryString = 'UPDATE artists SET name = $1 WHERE id = $2 RETURNING *' // Create a variable that will hold the SQL query, the name will come from the body while the id will come from the url. Both are handed into the function when called. RETURNING * is required for the query to return a result
-
-  try {
-
-    if (!artist.name) {
-      return // This will stop the function from executing if the user did not provide a name
-    }
-
-    const result = await pool.query(queryString, [artist.name, id]) // Use pool.query to send the query to the DB, name is taken from artist in the function argument, ID is taken from the id argument in function call
-    return result.rows[0] // Return the row from the result
-
-  } catch (error) {
-
-    throw new Error("There was an issue updating artist: " + error) // Handle any error if the request doesn't go through
-  
-  }
 }
 
 export async function deleteArtistById(id) {
   // Query the database to delete the artist and return the deleted artist or null
 
-  const queryString = 'DELETE FROM artists WHERE id = $1 RETURNING *' // Create a variable that will hold the SQL query, ID is taken from the function argument
-
-  try {
-
-    const result = await pool.query(queryString, [id]) // Use pool.query to send the query to the DB, ID is taken from the id argument in function call
-    return result.rows[0] // Return the row from the result
-
-  } catch (error) {
-
-    throw new Error("There was an issue deleting artist: " + error) // Handle any error if the request doesn't go through
   
-  }
 }
